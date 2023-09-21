410ff70c272e71f71888362a2aca075ced611f74
Daniel Lillo Barrado <97458634+Dessmondd@users.noreply.github.com>
2023-09-21 06:25:00 +0200

    clipboard bug fix
    clipboard bug fix

--- modules/clipboard_manager.py
+++ modules/clipboard_manager.py
@@ -2,18 +2,11 @@ import pyperclip
 import time 
 import threading 
 
-clear_duration = 30 # seconds 
-
 def clear_clipboard():
+    clear_duration = 30 # seconds 
     pyperclip.copy("")
-
-    while True:
-        clipboard_content = pyperclip.paste()
-        if clipboard_content:
-            time.sleep(clear_duration)
-            clear_clipboard()
 
 clear_clipboard_thread = threading.Thread(target=clear_clipboard)
 clear_clipboard_thread.daemon = True
-clear_clipboard_thread.start()
+
 
