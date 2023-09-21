26df28cfd5099f5c6ebd4f7f76c5497a9226a68e
Daniel Lillo Barrado <97458634+Dessmondd@users.noreply.github.com>
2023-09-21 06:24:25 +0200

    clipboard bug fix
    clipboard bug fix

--- main.py
+++ main.py
@@ -1,31 +1,19 @@
 import tkinter as tk
-from tkinter import messagebox
 from tkinter import messagebox, Toplevel, Listbox, Scrollbar, Button, MULTIPLE, END
 import secrets
 import string
 import pyperclip
-import os
-import random
 import tkinter.filedialog
 from modules.password_strength_tester import test_password
 from tkinter import ttk
 from ttkthemes import ThemedStyle
+from modules.wordlist import wordlist, vowels, consonants
+from modules.clipboard_manager import clear_duration, clear_clipboard
+import threading
 
 
-wordlist = [
-    "apple", "banana", "cherry", "date", "elder", "fig",
-    "grape", "honey", "kiwi", "lemon", "mango", "nectar",
-    "orange", "pear", "quince", "raspberry", "strawberry", "tangerine",
-    "watermelon", "blackberry", "blueberry", "cranberry", "grapefruit",
-    "kiwifruit", "pomegranate", "pineapple", "apricot", "blackcurrant",
-    "chocolate", "cinnamon", "vanilla", "ginger", "butterscotch",
-    "caramel", "marshmallow", "stracciatella", "peach", "plum", "lime",
-    "peppermint", "spearmint", "mango", "coconut", "passionfruit",
-    "blueberry", "vanilla", "hazelnut", "almond", "caramel",
-    "strawberry", "chocolate", "lemon", "cinnamon", "raspberry",
-]
 
-def generate_passphrase(num_words, add_numbers, add_special_chars, min_digits=None, max_digits=None, capitalize_percentage=60, include_spaces=True):
+def generate_passphrase(num_words, add_numbers, add_special_chars, min_digits=1, max_digits=4, capitalize_percentage=60, include_spaces=True):
     words = []
 
     for _ in range(num_words):
@@ -33,17 +21,11 @@ def generate_passphrase(num_words, add_numbers, add_special_chars, min_digits=No
         words.append(selected_word.capitalize())
 
     if add_numbers:
-        if not min_digits:
-            min_digits = secrets.randbelow(3) + 1
-        if not max_digits:
-            max_digits = secrets.randbelow(4) + 1
-
-    num_digits = secrets.randbelow(max(1, max_digits - min_digits + 1)) + min_digits
-    for _ in range(num_digits):
-        # Ensure that the upper bound for insertion is positive
-        insert_pos = secrets.randbelow(max(1, len(words) + 1))
-        words.insert(insert_pos, secrets.choice(string.digits))
-
+        num_digits = secrets.randbelow(max(max_digits - min_digits + 1, 1)) + min_digits
+        for _ in range(num_digits):
+            # Ensure that the upper bound for insertion is positive
+            insert_pos = secrets.randbelow(max(1, len(words) + 1))
+            words.insert(insert_pos, secrets.choice(string.digits))
 
     if add_special_chars:
         words.insert(secrets.randbelow(len(words) + 1), secrets.choice(string.punctuation))
@@ -61,6 +43,17 @@ def generate_passphrase(num_words, add_numbers, add_special_chars, min_digits=No
 
     return passphrase
 
+
+# Generate a pseudo-word by alternating between consonants and vowels, not implemented yet
+def generate_pseudo_word():
+    word_length = secrets.randint(3, 10)
+    word = ''
+    for i in range(word_length):
+        if i % 2 == 0:
+            word += secrets.choice(consonants)
+        else:
+            word += secrets.choice(vowels)
+    return word
 
 
 def toggle_spaces():
@@ -111,61 +104,70 @@ def generate_button_clicked():
 def copy_to_clipboard():
     passphrase = passphrase_label.cget("text").replace("Your new password is: ", "")
     pyperclip.copy(passphrase)
-
+    threading.Timer(clear_duration, clear_clipboard).start()
+      
 
 
 app = tk.Tk()
 app.title("Memorable Password Generator")
 app.resizable(width=False, height=False)
+
+
+
 style = ttk.Style()
-style.theme_use("xpnative") 
+style.theme_use("xpnative")
 
 
 num_words_label = ttk.Label(app, text="Enter the number of words in the passphrase:")
-num_words_label.pack()
+num_words_label.grid(row=0, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
 
 num_words_entry = ttk.Entry(app)
-num_words_entry.pack()
+num_words_entry.grid(row=0, column=2, padx=10, pady=5)
 
-# Checkbox for including numbers
+
 numbers_var = tk.BooleanVar()
 numbers_check = ttk.Checkbutton(app, text="Include a number", variable=numbers_var)
-numbers_check.pack()
+numbers_check.grid(row=1, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
-# Checkbox for including special characters
+
 special_chars_var = tk.BooleanVar(value=True)
 special_chars_check = ttk.Checkbutton(app, text="Include a special character", variable=special_chars_var)
-special_chars_check.pack()
+special_chars_check.grid(row=1, column=2, padx=10, pady=5, sticky="w")
 
 
-passphrase_label = ttk.Label(app, text="", wraplength=300)
-passphrase_label.pack()
-
-
-copy_button = ttk.Button(app, text="Copy to Clipboard", command=copy_to_clipboard, state=tk.DISABLED)
-copy_button.pack()
+include_spaces = tk.BooleanVar(value=True)  # Include spaces by default
+spaces_check = ttk.Checkbutton(app, text="Include Spaces", variable=include_spaces)
+spaces_check.grid(row=2, column=0, columnspan=2, padx=10, pady=5, sticky="w")
 
 
 load_custom_wordlists = ttk.Button(app, text="Load Custom Wordlist", command=load_custom_wordlist)
-load_custom_wordlists.pack()
-
-# Checkbox for including spaces
-include_spaces = tk.BooleanVar(value=True)  # Include spaces by default
-spaces_check = ttk.Checkbutton(app, text="Include Spaces", variable=include_spaces)
-spaces_check.pack()
+load_custom_wordlists.grid(row=2, column=2, padx=10, pady=5, sticky="w")
 
 
 generate_button = ttk.Button(app, text="Generate Memorable Passphrase", command=generate_button_clicked)
-generate_button.pack()
+generate_button.grid(row=3, column=0, columnspan=3, padx=10, pady=10)
+
+
+passphrase_label = ttk.Label(app, text="", wraplength=300)
+passphrase_label.grid(row=4, column=0, columnspan=3, padx=10, pady=5)
+
+
+copy_button = ttk.Button(app, text="Copy to Clipboard", command=copy_to_clipboard, state=tk.DISABLED)
+copy_button.grid(row=5, column=0, columnspan=3, padx=10, pady=5)
+
 
 strength_label = ttk.Label(app, text="", wraplength=300)
-strength_label.pack()
+strength_label.grid(row=6, column=0, columnspan=3, padx=10, pady=5)
 
 
 feedback_label = ttk.Label(app, text="", wraplength=300)
-feedback_label.pack()
+feedback_label.grid(row=7, column=0, columnspan=3, padx=10, pady=5)
 
-
+wordlist_choice = tk.StringVar(value="Static")  # Default choice is static
+static_radio = ttk.Radiobutton(app, text="Static Wordlist", variable=wordlist_choice, value="Static")
+static_radio.grid(row=8, column=0, columnspan=2, padx=10, pady=5, sticky="w")
+dynamic_radio = ttk.Radiobutton(app, text="Dynamic Wordlist", variable=wordlist_choice, value="Dynamic")
+dynamic_radio.grid(row=8, column=2, padx=10, pady=5, sticky="w")
 
 app.mainloop()
