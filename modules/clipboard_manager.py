38055dec0c6c23abe60f8633ca4dd70c2d2ba72a
Daniel <97458634+Dessmondd@users.noreply.github.com>
2024-01-18 17:50:51 +0100

    Add files via upload

--- modules/clipboard_manager.py
+++ modules/clipboard_manager.py
@@ -1,12 +1,43 @@
-import pyperclip 
-import time 
-import threading 
-clear_duration = 30 # seconds 
+import pyperclip
+import threading
+
+clear_duration = 5  # seconds
+clipboard_clear_flag = threading.Event()  
+clipboard_clear_thread = None  
+
 
 def clear_clipboard():
-    pyperclip.copy("")
+    global clipboard_clear_flag
+    while True:
+        clipboard_clear_flag.wait(clear_duration)
+        if clipboard_clear_flag.is_set():
+            pyperclip.copy("")
+            clipboard_clear_flag.clear()
+        else:
+            # If the flag is not set, break out of the loop (otherwise it crashes, software dev moment)
+            break 
 
-clear_clipboard_thread = threading.Thread(target=clear_clipboard)
-clear_clipboard_thread.daemon = True
+def start_clipboard_clear_thread():
+    global clipboard_clear_thread
+    clipboard_clear_thread = threading.Thread(target=clear_clipboard)
+    clipboard_clear_thread.daemon = True
+    clipboard_clear_thread.start()
 
-
+def stop_clipboard_clear_thread(force=False):
+    global clipboard_clear_thread
+    if clipboard_clear_thread and clipboard_clear_thread.is_alive():
+        # Clear the flag to stop clipboard clearing
+        clipboard_clear_flag.clear()  
+        clipboard_clear_thread.join()  
+        # Wait for the thread to finish before exiting
+        if force:
+            clipboard_clear_thread = None  
+        # Reset the clipboard clear thread if forced (need a revamp, works for now)
+def toggle_clipboard_clearing(state):
+    global clipboard_clear_flag
+    if state:
+        # Set the flag to signal clipboard clearing
+        clipboard_clear_flag.set()  
+    else:
+        # Clear the flag to stop clipboard clearing
+        clipboard_clear_flag.clear()  
